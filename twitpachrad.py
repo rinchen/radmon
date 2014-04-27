aebbb5b9afbb1076da677090b525adf698b1f1f1
Joey STANFORD <joey@stan4d.net>
2014-04-27 13:11:27 -0600

    ConfigParser + def main()
    Added basic config parser functionality mostly so that I don’t have to
    diff the program when I update the site. Moved main code into main()

--- twitpachrad.py
+++ twitpachrad.py
@@ -13,137 +13,173 @@ import time
 import sys
 import argparse
 import xively
+import ConfigParser
 
-parser = argparse.ArgumentParser(description='Receive Radiation'
-                                 ' data from arduino and post to'
-                                 ' twitter and xively.')
-parser.add_argument("-n", "--noop", action="store_true", dest="noop",
-                    help='''Do not post data online. Use with -v''',
-                    default=False)
-parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
-                    help='''Extra output regarding received values.''',
-                    default=False)
-args = parser.parse_args()
 
-# twitter
-encoding = None
-consumer_key = ''
-consumer_secret = ''
-access_token_key = ''
-access_token_secret = ''
+def readConfigFile(configfile):
+    config = ConfigParser.ConfigParser()
+    try:
+        config.read(configfile)
+    except:
+        print 'Config File Error'
+        exit(0)
+    return config
 
-twitter_api = twitter.Api(consumer_key=consumer_key,
-                          consumer_secret=consumer_secret,
-                          access_token_key=access_token_key,
-                          access_token_secret=access_token_secret,
-                          input_encoding=encoding)
 
-# xively
-X_API_KEY = ""
-X_FEED_ID = 12345
-x_api = xively.XivelyAPIClient(X_API_KEY)
-x_feed = x_api.feeds.get(X_FEED_ID)
+def main():
 
-#normally ttyACM0
-arduino = serial.Serial('/dev/ttyACM0', 19200)
-# for Mac OS X
-#arduino = serial.Serial('/dev/tty.usbmodem1d1341', 19200)
+    # twitter
+    encoding = None
+    consumer_key = config.get("Twitter", "consumer_key")
+    consumer_secret = config.get("Twitter", "consumer_secret")
+    access_token_key = config.get("Twitter", "access_token_key")
+    access_token_secret = config.get("Twitter", "access_token_secret")
 
-# we don't want to spam twitter
-twitter_counter = 0
+    twitter_api = twitter.Api(consumer_key=consumer_key,
+                              consumer_secret=consumer_secret,
+                              access_token_key=access_token_key,
+                              access_token_secret=access_token_secret,
+                              input_encoding=encoding)
 
-while True:
+    # xively
+    X_API_KEY = config.get("Xively", "X_API_KEY")
+    X_FEED_ID = config.get("Xively", "X_FEED_ID")
+    x_api = xively.XivelyAPIClient(X_API_KEY)
+    x_feed = x_api.feeds.get(X_FEED_ID)
 
-    # get the data and assemble it
-    message = arduino.readline()
-    message = message.strip().split(',')
-    if args.verbose:
-        print "Readline: %s\n" % message
+    # Arduino Serial port
+    arduino_serial = config.get("Arduino", "serial")
+    arduino_baud = config.get("Arduino", "baud")
+    arduino = serial.Serial(arduino_serial, arduino_baud)
 
-# On Mac OS X, there is a python-readline bug which will often
-# produce bogus data on the first pull. Until that bug is fixed
-# you'll need to include some sort of fix loop like this:
-#    try:
-#    #fix readline errors for usv
-#        if float(message[0]) < 200:
-#            if float(message[1]) > 20:
-#                message[1] = str(float(message[1]) / 100)
-#            if float(message[2]) > 20:
-#                message[2] = str(float(message[2]) / 100)
-#        twitter_message = message[0] + ' CPM, ' + message[1] + ' uSv/h, ' \
-#            + message[2] + ' AVG uSv/h, ' + message[3] \
-#            + ' time(s) over natural radiation'
-#        if args.verbose:
-#            print "Twitter: %s\n" % twitter_message
-#    except:
-#        print "Received bogus data"
-#        print "%s" % message
-#        continue
+    # we don't want to spam twitter
+    twitter_counter = 0
 
-# This is the original twitter message with all of the data. People will not
-# read the web page that describes how to interpret this and will start
-# panicking at the "times(s) over natural radiation". Because of this
-# we'll default to something less scary. The code is left here for your
-# reference.
-#        twitter_message = message[0] + ' CPM, ' + message[1] + ' uSv/h, ' \
-#            + message[2] + ' AVG uSv/h, ' + message[3] \
-#            + ' time(s) over natural radiation'
+    while True:
 
-# High radiation is anything over 100 milirems, aka 1000 uSv so let's
-# provide some hopefully helpful commentary on twitter.
-# We can use the on-board average to help filter out anomalies.
-# There is probably a better way to do this.
-#
-# Sometimes we get a back packet back resulting in us not having a
-# message[2].
-    if len(message) > 2:
-        usv_reading = float(message[1])
-        usv_average = float(message[2])
-        interpretation = ""
-        if usv_reading <= 1.2:
-            interpretation = "(normal range)"
-        elif usv_reading > 1.2 and usv_reading <= 250:
-            interpretation = "(slightly elevated)"
-        elif (usv_reading > 250 and usv_reading <= 499) \
-                and (usv_average > 250):
-            interpretation = "(Elevated reading)"
-        elif (usv_reading > 499 and usv_reading <= 999) \
-                and (usv_average > 499):
-            interpretation = "(Pre-Alarm! Significantly Elevated.)"
-        elif usv_reading > 999 and usv_average > 999:
-            interpretation = "(Radiation Alarm! [or the detector is broken])"
-        else:
-            interpretation = "(Disregard: failed quality control check)"
-
-        twitter_message = message[1] + ' uSv/h ' + interpretation
+        # get the data and assemble it
+        message = arduino.readline()
+        message = message.strip().split(',')
         if args.verbose:
-            print "Twitter: %s\n" % twitter_message
+            print "Readline: %s\n" % message
 
-        # send data to twitter every 10 minutes so we don't spam them
-        if (twitter_counter >= 10 and not args.noop):
+    # On Mac OS X, there is a python-readline bug which will often
+    # produce bogus data on the first pull. Until that bug is fixed
+    # you'll need to include some sort of fix loop like this:
+    #    try:
+    #    #fix readline errors for usv
+    #        if float(message[0]) < 200:
+    #            if float(message[1]) > 20:
+    #                message[1] = str(float(message[1]) / 100)
+    #            if float(message[2]) > 20:
+    #                message[2] = str(float(message[2]) / 100)
+    #        twitter_message = message[0] + ' CPM, ' \
+    #               + message[1] + ' uSv/h, ' \
+    #               + message[2] + ' AVG uSv/h, ' + message[3] \
+    #               + ' time(s) over natural radiation'
+    #        if args.verbose:
+    #            print "Twitter: %s\n" % twitter_message
+    #    except:
+    #        print "Received bogus data"
+    #        print "%s" % message
+    #        continue
+
+    # This is the original twitter message with all of the data.
+    # People will not
+    # read the web page that describes how to interpret this and will start
+    # panicking at the "times(s) over natural radiation". Because of this
+    # we'll default to something less scary. The code is left here for your
+    # reference.
+    #        twitter_message = message[0] + ' CPM, ' \
+    #            + message[1] + ' uSv/h, ' \
+    #            + message[2] + ' AVG uSv/h, ' + message[3] \
+    #            + ' time(s) over natural radiation'
+
+    # High radiation is anything over 100 milirems, aka 1000 uSv so let's
+    # provide some hopefully helpful commentary on twitter.
+    # We can use the on-board average to help filter out anomalies.
+    # There is probably a better way to do this.
+    #
+    # Sometimes we get a back packet back resulting in us not having a
+    # message[2]. And sometimes we just get a bad readline.
+        if len(message) > 2:
             try:
-                __ = twitter_api.PostUpdate(twitter_message)
+                usv_reading = float(message[1])
+                usv_average = float(message[2])
             except:
-                print "Twitter error", sys.exc_info()[0]
+                print "Malformed readline: %s" % (message)
             else:
-                twitter_counter = 0
+                interpretation = ""
+                if usv_reading <= 1.2:
+                    interpretation = "(normal range)"
+                elif usv_reading > 1.2 and usv_reading <= 250:
+                    interpretation = "(slightly elevated)"
+                elif (usv_reading > 250 and usv_reading <= 499) \
+                        and (usv_average > 250):
+                    interpretation = "(Elevated reading)"
+                elif (usv_reading > 499 and usv_reading <= 999) \
+                        and (usv_average > 499):
+                    interpretation = "(Pre-Alarm! Significantly Elevated.)"
+                elif usv_reading > 999 and usv_average > 999:
+                    interpretation = \
+                        "(Radiation Alarm! [or the detector is broken])"
+                else:
+                    interpretation = \
+                        "(Disregard: failed quality control check)"
 
-        #send data to xively
-        if not args.noop:
-            try:
-                x_feed.datastreams = [
-                    xively.Datastream(id='CPM', current_value=message[0]),
-                    xively.Datastream(id='USV', current_value=message[1]),
-                    xively.Datastream(id='USVAVG', current_value=message[2]),
-                    xively.Datastream(id='X', current_value=message[3]),
-                ]
-                x_feed.update()
-            except:
-                print "Xively error: %s" % (sys.exc_info()[0])
+                twitter_message = message[1] + ' uSv/h ' + interpretation
+                if args.verbose:
+                    print "Twitter: %s\n" % twitter_message
 
-        twitter_counter += 1
-    else:
-        print "Malformed readline: %s" % (message)
+                # send data to twitter every 10 minutes so we don't spam them
+                if (twitter_counter >= 10 and not args.noop):
+                    try:
+                        __ = twitter_api.PostUpdate(twitter_message)
+                    except:
+                        print "Twitter error: %s, Message: %s" % \
+                            (sys.exc_info()[0], message)
+                    else:
+                        twitter_counter = 0
 
-    # sleep for 1 minute
-    time.sleep(60)
+                #send data to xively
+                if not args.noop:
+                    try:
+                        x_feed.datastreams = [
+                            xively.Datastream(id='CPM',
+                                              current_value=message[0]),
+                            xively.Datastream(id='USV',
+                                              current_value=message[1]),
+                            xively.Datastream(id='USVAVG',
+                                              current_value=message[2]),
+                            xively.Datastream(id='X',
+                                              current_value=message[3]),
+                        ]
+                        x_feed.update()
+                    except:
+                        print "Xively error: %s" % (sys.exc_info()[0])
+
+            twitter_counter += 1
+        else:  # len(message)
+            print "Malformed readline: %s" % (message)
+
+        # sleep for 1 minute
+        time.sleep(60)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Receive Radiation'
+                                     ' data from arduino and post to'
+                                     ' twitter and xively.')
+    parser.add_argument("-n", "--noop", action="store_true", dest="noop",
+                        help='''Do not post data online. Use with -v''',
+                        default=False)
+    parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
+                        help='''Extra output regarding received values.''',
+                        default=False)
+    args = parser.parse_args()
+
+    configfile = "twitpatchrad.ini"
+
+    config = readConfigFile(configfile)
+
+    main()
